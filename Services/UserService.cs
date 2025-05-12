023d4fc254fb64d08c80bfe8a10d97a4c522a8f1
Y1243 <38328261243@mby.co.il>
2025-05-12 17:06:16 +0300

    Dependency Injection

---
+++ Services/UserService.cs
@@ -0,0 +1,25 @@
+﻿using Entities;
+using Reposirories;
+using System.Text.Json;
+
+
+namespace Services
+{
+    public class UserService
+    {
+        public string filePath = "M:\\web-api\\user.txt";
+        UserRepository userRepository = new UserRepository();
+        public User register(User user)
+        {
+            return userRepository.register(user);
+        }
+        public User login(User user)
+        {
+            return userRepository.login(user);
+        }
+        public User updateUser(int id, User userToUpdate)
+        {
+          return userRepository.updateUser(id, userToUpdate);
+        }
+    }
+}
