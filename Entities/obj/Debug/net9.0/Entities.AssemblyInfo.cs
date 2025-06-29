5600b7833851122b3f3688e0d3108ccd6e18ab3c
Y1243 <38328261243@mby.co.il>
2025-06-30 01:45:55 +0300

    open in home

--- Entities/obj/Debug/net9.0/Entities.AssemblyInfo.cs
+++ Entities/obj/Debug/net9.0/Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84b41c04568e896632d049a946137e75f8c63065")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cd48ce57f8f6667d63023026910b5b963e55837")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
